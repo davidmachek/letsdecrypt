2a7e632a45c96115eb117a7bf6a37281c6940c59
David Machek <165410431+davidmachek@users.noreply.github.com>
2025-04-13 16:58:36 +0200

    Update aes.py

--- aes.py
+++ aes.py
@@ -7,18 +7,20 @@ from Crypto.Util.Padding import pad, unpad
 from Crypto.Random import get_random_bytes
 import messagebox as messagebox
 import base64
-def encrypt(verbose):
+def encrypt(iv,verbose, message_decrypt, write_output, not_silence, save_decrypt, save_decrypt_file, key):
     try:
+            key = base64.b64decode(key)
+            iv = base64.b64decode(iv)
             if verbose:
                 messagebox.create_space_info("Decrypting with AES", write=write_output, print2=not_silence)
-                messagebox.space(f"Decrypting message: '{message_decrypt}' with key: '{key.decode()}'", write=write_output, print2=not_silence)
+
             encrypted_message = base64.b64decode(message_decrypt)
-            iv = encrypted_message[:AES.block_size]
-            ct = encrypted_message[AES.block_size:]
-            cipher = AES.new(key, AES.MODE_CBC, iv)
-            plaintext = unpad(cipher.decrypt(ct), AES.block_size).decode('utf-8')
+
+            cipher = AES.new(key, AES.MODE_CBC, iv)  # použije se tvoje iv
+            plaintext = unpad(cipher.decrypt(encrypted_message), AES.block_size).decode('utf-8')
+
             if verbose:
-                messagebox.end_space(f"The message was successfully decrypted!", write=write_output, print2=not_silence)
+                messagebox.end_space("The message was successfully decrypted!", write=write_output, print2=not_silence)
             messagebox.info(f"Decrypted Message: '{plaintext}'", write=write_output, print2=True)
 
             if save_decrypt:
